5aaa5011802f06688db0fc1e2df00de049b459e8
Alexeivalley <00080525@uca.edu.sv>
2025-08-14 12:33:39 -0600

    :sparkles: feat: New feature

--- clase_0.cpp
+++ clase_0.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <cmath>
 
-int main()
-{
-    std::cout << "Mi primer snipper" << std::endl;
+const float kPi = 3.1416;
+
+float CalcularAreaCirculo(float radio){
+    return kPi * (radio * radio);
+}
+
+int main() {
+    float r;
+    std::cout << "Ingrese el radio del circulo:" << std::endl;
+    std::cin >> r;
+    std::cout << "El area del circulo es: " << round(CalcularAreaCirculo(r) * 100) / 100 << std::endl;
     return 0;
 }
+
+//tarea: hacer que solo se impriman 2 decimales
